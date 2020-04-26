fe30c89282a9bdf750ac23dc5099b8f867ed72a1
Sway <sway@Sways-MacBook-Pro.local>
2020-04-26 18:48:08 -0500

    Updated with addition methods

--- os.py
+++ os.py
@@ -1,6 +1,7 @@
+#os.walk(path) --> lists out directories and subdirs
 import os
 
 pwd = os.getcwd()
-list_directory = os.listdir(pwd)
+list_directory = os.walk(pwd)
 for directory in list_directory:
     print (directory)
